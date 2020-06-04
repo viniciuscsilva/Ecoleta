e13b9834f034a41233acb7055da032207ea942bd
Vinícius de Castro <viniciuscsilva4@gmail.com>
2020-06-04 15:06:02 -0300

    Atualização do dia 4. Criação do servidor, ajustes com o Json e mais

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const server = express()
+
+
+// Configurar pasta publica
+server.use(express.static("public"))
+
+
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+// Configurar os caminhos da aplicação
+// Página inicial
+//req: Requisição
+//res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", {tittle: "Um título"})
+})
+
+server.get("/create-point", (req, res) => {
+   return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+ })
+
+
+// Ligar o servidor
+server.listen(3000)
